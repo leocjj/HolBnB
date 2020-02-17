612774decfec8027166153c3801df7e817668c35
Leo CJ <53725367+leocjj@users.noreply.github.com>
2020-02-17 17:57:10 -0500

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" User class. Used as base for future users."""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User class. Used as base for future users."""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
